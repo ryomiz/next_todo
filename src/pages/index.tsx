b6fc2b12daf239660a72811b9120d1a96bde4b8c
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2021-10-04 08:19:31 +0900

    [追加]不要ファイル削除 && tailwindcss追加

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className="container mx-auto mt-8 p-6 shadow text-2xl">
+      Hello World
+    </div>
+  );
+};
+
+export default Home;
