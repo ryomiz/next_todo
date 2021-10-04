879ea0ae33c5f810f2a57463087de30c7b6642db
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2021-10-04 09:02:09 +0900

    [追加]headless uiの追加

---
+++ src/pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html lang="ja">
+        <Head>
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,43 @@
+import Image from 'next/image'
+
 import type { NextPage } from 'next'
+
+const people = [
+  {
+    name: 'Calvin Hawkins',
+    email: 'calvin.hawkins@example.com',
+    image:
+      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Kristen Ramos',
+    email: 'kristen.ramos@example.com',
+    image:
+      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Ted Fox',
+    email: 'ted.fox@example.com',
+    image:
+      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+]
+
 const Home: NextPage = () => {
   return (
-    <div className="container mt-8 mx-auto p-6 text-2xl shadow">
-      Hello World
-    </div>
+    <ul className="divide-gray-200 divide-y">
+      {people.map((person) => (
+        <li key={person.email} className="flex p-4">
+          <div className="w-10 h-10 rounded-full overflow-hidden">
+            <Image width={256} height={256} src={person.image} alt="" />
+          </div>
+          <div className="ml-3">
+            <p className="text-gray-900 text-sm font-medium">{person.name}</p>
+            <p className="text-gray-500 text-sm">{person.email}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
   )
 }
 
