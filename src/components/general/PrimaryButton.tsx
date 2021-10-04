bd554a7671acde675cfb9fc84414530301c697e7
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2021-10-04 20:16:07 +0900

    [追加]PrimaryButtonの作成

---
+++ src/components/general/PrimaryButton.tsx
@@ -0,0 +1,21 @@
+type Props = {
+  children: React.ReactNode
+  isLarge?: boolean
+}
+
+export const PrimaryButton: React.FC<Props> = (props) => {
+  const { children, isLarge = false } = props
+
+  if (isLarge) {
+    return (
+      <button className="px-8 py-3 text-white text-xl bg-pink-400 hover:bg-pink-500 rounded-lg">
+        {children}
+      </button>
+    )
+  }
+  return (
+    <a className="px-6 py-2 text-white text-sm bg-pink-400 hover:bg-pink-500 rounded-lg">
+      {children}
+    </a>
+  )
+}
