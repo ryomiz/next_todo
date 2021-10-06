60b0ff78873a2513e4c9568f5bb88cbf6c371e58
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2021-10-07 02:57:03 +0900

    [追加]formatDate関数追加 && 微修正

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -4,6 +4,6 @@ import type { Task } from 'src/types'
 
 export const fetcher = async (): Promise<Array<Task>> => {
   const res = await axiosInstance.get('/')
-  const data = await res.data
+  const data = res.data
   return data
 }
