944ca2c070eaedc0f0f807bd1155d69ae1be5066
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2021-10-07 23:17:59 +0900

    [追加]React-beautiful-dnd追加、TaskListで実装
    DB設計をやり直さないときつい...

--- src/components/DiscardedTaskList.tsx
+++ src/components/DiscardedTaskList.tsx
@@ -14,11 +14,11 @@ export const DiscardedTaskList: React.VFC = () => {
       <h2 className="mb-4 text-center text-red-900 text-xl">
         削除されたタスク
       </h2>
-      <div className="flex flex-col gap-4 mx-auto w-6/12">
+      <ul className="flex flex-col gap-4 mx-auto w-6/12">
         {discarded.map((task) => (
           <DiscardedTask key={task.id} task={task} />
         ))}
-      </div>
+      </ul>
     </div>
   )
 }
