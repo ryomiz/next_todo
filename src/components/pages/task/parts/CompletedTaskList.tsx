f398b5ffc56e4141802aae448b4c39bd3c376b09
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2021-10-10 19:16:05 +0900

    [修正]react-beautiful-dndの削除

--- src/components/pages/task/parts/CompletedTaskList.tsx
+++ src/components/pages/task/parts/CompletedTaskList.tsx
@@ -1,57 +1,19 @@
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
-
 import { CompletedTask } from 'src/components/general/task/CompletedTask'
 import { useSetTask } from 'src/hooks/useSetTask'
 
 export const CompletedTaskList: React.VFC = () => {
-  const { completed, setUncompleted } = useSetTask()
-
-  const onDragEnd = async (result: any) => {
-    try {
-      const targetId = Number(result.draggableId)
-      const targetTask = completed.find((tsk) => tsk.id === targetId)!
-      const destination = Number(result.destination.index)
-      const newUncompleted = [...completed].filter((tsk) => tsk.id !== targetId)
-      newUncompleted.splice(destination, 0, targetTask)
-      setUncompleted(newUncompleted)
-    } catch (err) {
-      return
-    }
-  }
+  const { completed } = useSetTask()
 
   return (
     <div className="mb-8 rounded-lg">
       <h2 className="mb-4 text-red-600 text-xl">完了したタスク</h2>
-      <DragDropContext onDragEnd={() => onDragEnd('')}>
-        <Droppable droppableId="uncompleted">
-          {(provided) => (
-            <ul
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-              className="flex flex-col gap-4"
-            >
-              {completed.map((task, index: number) => (
-                <Draggable
-                  key={task.id}
-                  draggableId={task.id.toString()}
-                  index={index}
-                >
-                  {(provided) => (
-                    <li
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      ref={provided.innerRef}
-                    >
-                      <CompletedTask task={task} />
-                    </li>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </ul>
-          )}
-        </Droppable>
-      </DragDropContext>
+      <ul className="flex flex-col gap-4">
+        {completed.map((task) => (
+          <li key={task.id}>
+            <CompletedTask task={task} />
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
--- src/components/pages/task/parts/TaskList.tsx
+++ src/components/pages/task/parts/TaskList.tsx
@@ -6,36 +6,10 @@ import { UncompletedTaskList } from './UncompletedTaskList'
 import { useSetTask } from 'src/hooks/useSetTask'
 
 export const TaskList: React.VFC = () => {
-  const { uncompleted, completed, setUncompleted, setData } = useSetTask()
-
+  const { setData } = useSetTask()
   useEffect(() => {
     setData()
   }, [])
-
-  // const onDragStart = (result: any) => {
-  //   console.log('start')
-  // }
-  // const onDragUpdate = (result: any) => {
-  //   console.log('update')
-  //   console.log(result)
-  //   console.log(uncompleted)
-  // }
-
-  const onDragEnd = async (result: any) => {
-    try {
-      const targetId = Number(result.draggableId)
-      const targetTask = uncompleted.find((tsk) => tsk.id === targetId)!
-      const destination = Number(result.destination.index)
-      const newUncompleted = [...uncompleted].filter(
-        (tsk) => tsk.id !== targetId
-      )
-      newUncompleted.splice(destination, 0, targetTask)
-      setUncompleted(newUncompleted)
-    } catch (err) {
-      return
-    }
-  }
-
   return (
     <div className="flex flex-col gap-6 pt-20 w-6/12">
       <UncompletedTaskList />
