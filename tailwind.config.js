53024c3ff8b2b20ab67f5fa5d2ee7c7242fb75ff
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2021-10-18 00:40:52 +0900

    [修正]custom hooksからreturnされるコンポーネントのstyleもpurge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ['./src/components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/hooks/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
